eacc76350aef32b5de250dcca4ea0426cc6e57c1
Nanling <neithern@outlook.com>
2023-06-01 21:42:44 +0800

    remove non-existent definitions

--- src/gst/gst-ext.h
+++ src/gst/gst-ext.h
@@ -7,12 +7,6 @@ G_BEGIN_DECLS
 
 GstTagList * ape_demux_parse_tags (const guint8 * data, gint size);
 
-void gst_level_calculate_gint8   (gpointer data, guint num, guint channels, gdouble *NCS, gdouble *NPS);
-void gst_level_calculate_gint16  (gpointer data, guint num, guint channels, gdouble *NCS, gdouble *NPS);
-void gst_level_calculate_gint32  (gpointer data, guint num, guint channels, gdouble *NCS, gdouble *NPS);
-void gst_level_calculate_gfloat  (gpointer data, guint num, guint channels, gdouble *NCS, gdouble *NPS);
-void gst_level_calculate_gdouble (gpointer data, guint num, guint channels, gdouble *NCS, gdouble *NPS);
-
 G_END_DECLS
 
 #endif /* __GST_EXT_H__ */
